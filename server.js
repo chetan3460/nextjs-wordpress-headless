20576e101f2d8f55bac3286b280fdb644d502950
chetan dhargalkar <chetan.dmc@gmail.com>
2026-04-02 16:24:49 +0530

    fix: initialize Next.js app with app.prepare() to ensure build manifests are loaded correctly

--- server.js
+++ server.js
@@ -9,17 +9,25 @@ const handle = app.getRequestHandler();
 
 const port = process.env.PORT || 3000;
 
-// Skip app.prepare() to save critical resources on shared hosting
-createServer(async (req, res) => {
-  try {
-    const parsedUrl = parse(req.url, true);
-    await handle(req, res, parsedUrl);
-  } catch (err) {
-    console.error('Error occurred handling', req.url, err);
-    res.statusCode = 500;
-    res.end(`Internal Server Error: ${err.message}\nStack: ${err.stack}`);
-  }
-}).listen(port, err => {
-  if (err) throw err;
-  console.log(`> Ready on http://localhost:${port}`);
-});
+// prepare() is mandatory for custom servers to load build manifests
+app
+  .prepare()
+  .then(() => {
+    createServer(async (req, res) => {
+      try {
+        const parsedUrl = parse(req.url, true);
+        await handle(req, res, parsedUrl);
+      } catch (err) {
+        console.error('Error occurred handling', req.url, err);
+        res.statusCode = 500;
+        res.end(`Internal Server Error: ${err.message}\nStack: ${err.stack}`);
+      }
+    }).listen(port, err => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Failed to prepare Next.js app', err);
+    process.exit(1);
+  });
